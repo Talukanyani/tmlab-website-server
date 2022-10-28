87fe6d6e5d4617e86a5a3fe76c46cb6887bdc4b2
Talukanyani Mutshaeni <tmutshaeni@hotmail.com>
2022-10-29 00:17:06 +0200

    deployment

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,27 +1,24 @@
 // Database for development
 const devDB = {
     host: 'localhost',
-    port: 3306,
     user: 'talu',
     password: 'taluSlocalDB@2022',
-    database: 'tmlab',
+    database: 'tmlab'
 }
 
 // Database for production
 const prodDB = {
     host: process.env.RDS_HOSTNAME,
-    port: process.env.RDS_PORT,
     user: process.env.RDS_USERNAME,
     password: process.env.RDS_PASSWORD,
-    database: process.env.RDS_DB_NAME,
+    database: process.env.RDS_DB_NAME
 }
 
 const dbConfig = {
     host: prodDB.host,
-    port: prodDB.port,
     user: prodDB.user,
     password: prodDB.password,
-    database: prodDB.database,
+    database: prodDB.database
 }
 
 module.exports = dbConfig
